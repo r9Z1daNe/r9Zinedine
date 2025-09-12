da947003429db169440e1c5fc150ec1c26211d51
r9Z1daNe <samuel.cruz21delvalle@gmail.com>
2025-09-12 12:23:05 -0600

    Update sketch.js

--- 02 user events/sketch.js
+++ 02 user events/sketch.js
@@ -14,14 +14,30 @@ function draw() {
   challenge(); // coordinate system challenge
 }
 
+function KeyPressed(){
+//this is a special EVENT function, gets
+//automatically called anytime keyboard
+//button is pressed.
+  print("key was pressed")
+  circleColor = !circleColor;
+
+// how to tell which key was pressed???
+}
+
+
 function challenge(){
   //draw 5 hollow circles, in 4 corners and
   //center position, 50px in diameter each
   nofill();
+
+  if(circleColor === true) {    //circlecolor ===
+    fill(255,0,0);
+  }
 
   // 5 circles
   circle(0,0,50);
   circle(width,0,50);
   circle(0,height,50);
   circle(width,height,50);
+  circle(width*0.5)
 }
