84128a166aa4206688739e525b694909a82c3de4
r9Z1daNe <samuel.cruz21delvalle@gmail.com>
2025-09-15 13:37:31 -0600

    Multiple Keys and outlines demo

---
+++ 003/sketch.js
@@ -0,0 +1,30 @@
+// Multiple Keys and outlines
+// Sam
+// September 15 2025
+//
+// Extra for Experts:
+//KeyisDown
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  checkMulti();
+}
+
+
+function checkMulti(){
+  strokeWeight(mouseX / 10);
+  stroke(255,0,0);
+  //check for multiple keypresses (3 simultaneous)
+  let a = keyIsDown(65); //"a"
+  let b = keyIsDown(66); //"b"
+  let c = keyIsDown(67); //"c"
+  textSize(40);
+  text("a:" + a + "\tb:" + b + "\tc:" + c, 100,300);
+
+
+}
