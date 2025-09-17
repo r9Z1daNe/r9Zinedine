dbdb7690b5a1797526fb0bfe7d216e24b386a771
r9Z1daNe <samuel.cruz21delvalle@gmail.com>
2025-09-17 14:03:58 -0600

    interactive scene

---
+++ assigments/interactive scene/sketch.js
@@ -0,0 +1,26 @@
+// Interactive Scene
+// Sam
+// 9/17/2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(800, 1000);
+}
+function draw() {
+  background(20, 163, 182);
+
+
+}
+
+function circle () {
+  createCanvas(100, 100);
+
+  background(150);
+
+  circle(50, 50, 25);
+
+  describe('A white circle with black outline in the middle of a gray canvas.');
+}
