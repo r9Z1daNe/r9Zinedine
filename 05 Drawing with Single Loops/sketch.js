ac46fbe420a012685907bc6c2a5e46c7821f33a1
r9Z1daNe <samuel.cruz21delvalle@gmail.com>
2025-09-23 14:09:21 -0600

    circle line demo

---
+++ 05 Drawing with Single Loops/sketch.js
@@ -0,0 +1,71 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+  gradientBackground();
+
+  circleLine(height *0.35, 30);
+  circleLine(height/2, 50);
+  circleLine(height * 0.65, 80);
+}
+
+function gradientBackground(){
+  //create a gradient to use as a background
+  let h = 3;  //height of each rectangle
+
+  //use a loop (doesn't have to be WHILE) to 
+  //draw a vertical stack of rectangles
+  let y = 0;
+  while (y <= height){
+    noStroke();
+    let mappedY = map(y,0,height,0,255);
+    let flippedY = 255 - mappedY;
+    let mappedMouseX = map(mouseX, 0, width, 0,255);
+    let mappedMouseY = map(mouseY, 0, height, 0,255);
+    fill(mappedMouseY, flippedY, mappedMouseX);
+    rect(0, y, width, h);
+    y += h;
+  }
+
+}
+
+
+
+
+function cDistance(x1, y1, x2, y2 ){
+  //calculate the straightline distance
+  //between (x1, y1) and (x2, y2)
+  let a = abs(x1 - x2); 
+  let b = abs(y1 - y2); //c = sqrt(a^2 + b^2)
+  let c = sqrt(pow(a,2) + pow(b,2));
+  return c.toFixed(1); //keep only 1 decimal place
+}
+
+function circleLine(y, size){
+  // use this function to draw a line of circles (loop)
+  // y → number   the height at which to draw the line
+  // size → number   diameter of the circles
+  let xStart = width * 0.1;  //10% position from the left
+  let xEnd = width * 0.9;    //90% horizontal pos from left
+
+  for(let x = xStart ; x <= xEnd ; x += size){
+    let d = cDistance(x,y,mouseX, mouseY);
+    if(d <= size/2){ //dist less than radius, IN CIRCLE
+      fill(200,200,0);
+    }
+    else fill(255);
+    circle(x, y, size);
+    textAlign(CENTER, CENTER);
+    fill(0);
+    text(d, x, y);
+  }
+}
